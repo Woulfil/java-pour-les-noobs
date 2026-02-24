912741f1bd1c37919c03a403a04944331b0bdfa2
Pierre B. <119108037+Woulfil@users.noreply.github.com>
2026-02-24 11:57:33 +0000

    Ajout des méthodes pour gérer les livres dans la classe Library et implémentation des getters et de toString dans la classe Book

--- library/Book.java
+++ library/Book.java
@@ -5,8 +5,32 @@ public class Book {
     private int year;
 
     // TODO 1 : Créer un constructeur
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
 
     // TODO 2 : Créer les getters
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
 
     // TODO 3 : Redéfinir toString()
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", year=" + year +
+                '}';
+    }
 }
--- library/Library.java
+++ library/Library.java
@@ -6,6 +6,29 @@ public class Library {
     private List<Book> books = new ArrayList<>();
 
     // TODO 4 : Ajouter un livre
+    public void addBook(Book book) {
+        if (book != null) {
+            books.add(book);
+        }
+    }
+
     // TODO 5 : Afficher tous les livres
+    public void displayBooks() {
+        for (Book book : books) {
+            System.out.println(book);
+        }
+    }
+
     // TODO 6 : Rechercher un livre par titre
+    public Book findBookByTitle(String title) {
+        if (title == null) {
+            return null;
+        }
+        for (Book book : books) {
+            if (title.equals(book.getTitle())) {
+                return book;
+            }
+        }
+        return null;
+    }
 }
